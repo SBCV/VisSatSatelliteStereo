7db1aa644263aec02751b91787b2a40ef05e762e
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2025-02-19 13:04:14 +0100

    Add reformat_code.sh

---
+++ reformat_code.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Go to the directory where the script is located
+cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
+REQUIRED_VERSION="24.3.0"
+black --required-version ${REQUIRED_VERSION} --line-length 79 colmap_sfm_commands.py
